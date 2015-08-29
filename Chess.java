4bf4b01a8766c19e958a7ef7040d3b96aa9721bb
Arieh Smith <arieh.smith@gmail.com>
2015-08-29 11:27:36 -0400

    Game loop lets you move

--- Chess.java
+++ Chess.java
@@ -1,10 +1,35 @@
 package chess;
+import java.util.Scanner;
 
 public class Chess {
 
     public static void main(String[] args) {
         Board board = new Board();
-        System.out.println(board.toString());
+        String color = "B";
+        Scanner reader = new Scanner(System.in);
+        
+        // Game loop
+        while (true) {
+            // Swap colors
+            color = (color.equals("W") ? "B" : "W");
+            
+            // Print the board
+            System.out.println(board.toString());
+                        
+            // Keep looping while the user makes a valid move
+            String from = "H0"; String to = "H0";
+            
+            while (!board.isValidMoveForColor(from, to, color)) {
+                System.out.print(color.equals("W") ? "White" : "Black");
+                System.out.println(", please enter a valid move (e.g. E2E4):");
+                String move = reader.next();
+                from = move.substring(0, 2);
+                to   = move.substring(2);
+            }
+            
+            // Make the move
+            board.move(from, to);
+        }
     }
     
 }
