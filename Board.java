8b450867e34dee763e716a0177b16a114c4ab44e
Arieh Smith <arieh.smith@gmail.com>
2015-08-30 20:51:40 -0400

    Board knows valid moves for square

--- Board.java
+++ Board.java
@@ -38,6 +38,8 @@ public class Board {
     }
     
     public boolean isValidMoveForColor(String from, String to, String color) {
+        // TODO: castling, en passant
+        
         if (from.equals(to)) return false;
         
         Square fromSquare = getSquareAtCoordinates(from);
@@ -48,9 +50,9 @@ public class Board {
 
         if (!fromSquare.isOccupiedByPieceOfColor(color)) return false;
         if (toSquare.isOccupiedByPieceOfColor(color)) return false;
-
-        return true;
         
+        if (validMovesForSquare(fromSquare).contains(toSquare)) return true;
+        else return false;
     }
     
     public void move(String from, String to) {
@@ -64,11 +66,113 @@ public class Board {
         int col = Columns.indexOf(coords.substring(0, 1));
         int row = 8 - Integer.parseInt(coords.substring(1, 2));
 
-        if (col < 0 || col > 7 || row < 0 || row > 7) return null;
+        if (!rowAndColAreWithinBounds(row, col)) return null;
         
         return Board[row][col];
     }
     
+    private boolean rowAndColAreWithinBounds(int row, int col) {
+        return col >= 0 && col <= 7 && row >= 0 && row <= 7;
+    }
+    
+    public List<Square> validMovesForSquare(Square square) {
+        Piece piece = square.getPiece();
+        if (piece == null) return null;
+        
+        List<Square> validMoves = new ArrayList<Square>();
+        
+        // Special moves the piece is a pawn or a knight
+        if (piece.canMoveLikePawn()) return validMovesForPawn(square);
+        if (piece.canMoveLikeKnight()) return validMovesForKnight(square);
+        
+        // Add all normal directional moves
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesUp(), 1, 0, 0, 0));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesSide(), 0, 1, 0, 0));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesDown(), 0, 0, 1, 0));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesSide(), 0, 0, 0, 1));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesDiagonal(), 1, 1, 0, 0));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesDiagonal(), 1, 0, 0, 1));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesDiagonal(), 0, 1, 1, 0));
+        validMoves.addAll(validMovesForSquareInDirection(square, piece.movesDiagonal(), 0, 0, 1, 1));
+        
+        return validMoves;
+    }
+    
+    private List<Square> validMovesForSquareInDirection(Square square, int length, int up, int right, int down, int left) {
+        List<Square> validMoves = new ArrayList<Square>();
+        for (int i = 1; i <= length; i++) {
+            if (up != 0) up += i - 1;
+            if (down != 0) down += i - 1;
+            if (right != 0) right += i - 1;
+            if (left != 0) left += i - 1;
+            int row = square.Row - up + down;
+            int col = square.Col + right - left;
+            if (!rowAndColAreWithinBounds(row, col)) break;
+            Square s = Board[row][col];
+            if (s.isOccupied()) break;
+            validMoves.add(s);
+        }
+        return validMoves;
+    }
+    
+    private List<Square> validMovesForKnight(Square square) {
+        List<Square> validMoves = new ArrayList<Square>();
+        
+        // Up two, right one. Up two, left one.
+        // Down two, right one. Down two, left one.
+        // Right two, up one. Right two, down one.
+        // Left two, up one. Left two, down one.        
+        
+        int rowColPairs[][] = {
+            {-2,1}, {-2,-1},
+            {2,1}, {2,-1},
+            {-1, 2}, {1, 2},
+            {-1, -2}, {1, -2}
+        };
+
+        for (int i = 0; i < rowColPairs.length; i++) {
+            int row = square.Row + rowColPairs[i][0];
+            int col = square.Col + rowColPairs[i][1];
+            if (rowAndColAreWithinBounds(row, col)) {
+                Square s = Board[row][col];
+                if (!s.isOccupiedByPieceOfColor(square.getPiece().Color))
+                    validMoves.add(s);
+            }
+        }
+
+        return validMoves;
+    }
+    
+    public List<Square> validMovesForPawn(Square square) {
+        List<Square> validMoves = new ArrayList<Square>();
+        
+        // Regular moves, 1 or 2 up or down depending on color
+        Piece piece = square.getPiece();
+        if (piece.isWhite()) {
+            int movesUp = piece.movesUp();
+            if (square.containsPawnOnStartingRow()) movesUp += 1;
+            validMoves.addAll(validMovesForSquareInDirection(square, movesUp, 1, 0, 0, 0));   
+        } else {
+            int movesDown = piece.movesDown();
+            if (square.containsPawnOnStartingRow()) movesDown += 1;
+            validMoves.addAll(validMovesForSquareInDirection(square, movesDown, 0, 0, 1, 0));   
+        }
+        
+        // Attacking move
+        int rowColPairs[][] = { {-1,-1}, {-1,1} };
+        for (int i = 0; i < rowColPairs.length; i++) {
+            int row = square.Row + rowColPairs[i][0];
+            int col = square.Col + rowColPairs[i][1];
+            if (rowAndColAreWithinBounds(row, col)) {
+                Square s = Board[row][col];
+                if (s.isOccupiedByPieceOfColor(square.getPiece().oppositeColor()))
+                    validMoves.add(s);
+            }
+        }
+
+        return validMoves;
+    }
+        
     public String toString() {
         String board = "";
         String horizontalLabel = "  A B C D E F G H\n";
