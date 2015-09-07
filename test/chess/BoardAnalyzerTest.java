9ca92cc1de328d51833d3a2ffdcdf68756cafe8e
Arieh Smith <arieh.smith@gmail.com>
2015-09-07 14:50:31 -0400

    Beef up board analysis tests

--- test/chess/BoardAnalyzerTest.java
+++ test/chess/BoardAnalyzerTest.java
@@ -34,14 +34,36 @@ public class BoardAnalyzerTest {
     }
 
     @Test
-    public void testPointValueInStartingPosition() {
+    public void testPointValue() {
         Board board = new Board();
         BoardAnalyzer instance = new BoardAnalyzer();
         HashMap<String, Double> expResult = new HashMap();
+        
+        // Starting position
         expResult.put("W", 43.5);
         expResult.put("B", 43.5);
         HashMap<String, Double> result = instance.pointValueForBoardAndColor(board);
         assertEquals(expResult, result);
+        
+        // After one pawn capture
+        board.move("E2", "E4");
+        board.move("D7", "D5");
+        board.move("E4", "D5");
+        expResult.put("B", 42.5);
+        result = instance.pointValueForBoardAndColor(board);
+        assertEquals(expResult, result);
+        
+        // After bishop capture
+        board.move("C8", "E6");
+        board.move("D5", "E6");
+        expResult.put("B", 39.25);
+        result = instance.pointValueForBoardAndColor(board);
+        assertEquals(expResult, result);
+        
+        // After pawn takeback
+        board.move("F7", "E6");
+        expResult.put("W", 42.5);
+        result = instance.pointValueForBoardAndColor(board);
+        assertEquals(expResult, result);
     }
-    
 }
